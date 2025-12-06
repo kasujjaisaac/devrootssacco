06017cb560ee32cd88ab74266666f08b400336d9
isaackasujja <kasujjaisaac19@gmail.com>
2025-12-06 13:33:38 +0300

    added neon database

--- sacco_backend/settings.py
+++ sacco_backend/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # ============================
 # BASE SETTINGS
@@ -78,12 +79,23 @@ WSGI_APPLICATION = 'sacco_backend.wsgi.application'
 # ============================
 # DATABASE
 # ============================
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Use PostgreSQL on Render for production
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # ============================
 # PASSWORD VALIDATORS
